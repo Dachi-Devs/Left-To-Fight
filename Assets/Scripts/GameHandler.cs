42e2542ab551e2970750d6496cc3d9bbe58590d8
Alex Sampson <32361325+15015483Alex@users.noreply.github.com>
2020-03-11 16:46:00 +0000

    Sprite Starting

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    [SerializeField]
+    private Texture2D baseTexture;
+    [SerializeField]
+    private Texture2D headTexture;
+    [SerializeField]
+    private Texture2D bodyTexture;
+    [SerializeField]
+    private Material humanMaterial;
+
+    private void Awake()
+    {
+        Texture2D texture = new Texture2D(512, 512, TextureFormat.RGBA32, true);
+
+        Color[] spriteSheetBasePixels = baseTexture.GetPixels(0, 0, 512, 512);
+        texture.SetPixels(0, 0, 512, 512, spriteSheetBasePixels);
+
+        Color[] headPixels = headTexture.GetPixels(0, 0, 128, 128);
+        texture.SetPixels(0, 384, 128, 128, headPixels);
+
+        Color[] bodyPixels = bodyTexture.GetPixels(0, 0, 128, 128);
+        texture.SetPixels(0, 256, 128, 128, bodyPixels);
+
+        texture.Apply();
+
+        humanMaterial.mainTexture = texture;
+    }
+}
