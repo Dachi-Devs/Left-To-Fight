c928306116e86f0b385b1b6c522dab549bdbddda
Alex Sampson <32361325+15015483Alex@users.noreply.github.com>
2020-03-12 23:39:15 +0000

    Random Sprite

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -15,15 +15,21 @@ public class GameHandler : MonoBehaviour
 
     private void Awake()
     {
+        const int HEAD_DIMENSIONS = 128;
+        const int BODY_DIMENSIONS = 128;
+
+        int headPixel = Random.Range(0, headTexture.width / HEAD_DIMENSIONS) * HEAD_DIMENSIONS;
+        int bodyPixel = Random.Range(0, bodyTexture.width / BODY_DIMENSIONS) * BODY_DIMENSIONS;
+
         Texture2D texture = new Texture2D(512, 512, TextureFormat.RGBA32, true);
 
         Color[] spriteSheetBasePixels = baseTexture.GetPixels(0, 0, 512, 512);
         texture.SetPixels(0, 0, 512, 512, spriteSheetBasePixels);
 
-        Color[] headPixels = headTexture.GetPixels(0, 0, 128, 128);
+        Color[] headPixels = headTexture.GetPixels(headPixel, 0, HEAD_DIMENSIONS, HEAD_DIMENSIONS);
         texture.SetPixels(0, 384, 128, 128, headPixels);
 
-        Color[] bodyPixels = bodyTexture.GetPixels(0, 0, 128, 128);
+        Color[] bodyPixels = bodyTexture.GetPixels(bodyPixel, 0, BODY_DIMENSIONS, BODY_DIMENSIONS);
         texture.SetPixels(0, 256, 128, 128, bodyPixels);
 
         texture.Apply();
