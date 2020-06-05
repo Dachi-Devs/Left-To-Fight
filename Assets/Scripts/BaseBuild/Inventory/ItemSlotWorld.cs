9803fc4ea003be8e2621aa99d0b2d7c975025021
Alex Sampson <32361325+AlexSDevDump@users.noreply.github.com>
2020-06-05 11:16:53 +0100

    Item Drop Test

--- Assets/Scripts/BaseBuild/Inventory/ItemSlotWorld.cs
+++ Assets/Scripts/BaseBuild/Inventory/ItemSlotWorld.cs
@@ -7,7 +7,28 @@ public class ItemSlotWorld : MonoBehaviour
     [SerializeField]
     private ItemSlot itemSlot;
 
-    public void SetItemSlot(ItemSlot itemSlot) => this.itemSlot = itemSlot;
+    [SerializeField]
+    private string humanTag;
+
+    private void Start()
+    {
+        SetItemSlot(itemSlot);
+    }
+
+    public void SetItemSlot(ItemSlot itemSlot)
+    {
+        this.itemSlot = itemSlot;
+        GetComponent<SpriteRenderer>().sprite = this.itemSlot.item.sprite;
+    }
 
     public ItemSlot GetItemSlot() => itemSlot;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == humanTag)
+        {
+            collision.gameObject.GetComponent<PlayerInventory>().inventory.AddItem(itemSlot);
+            Destroy(gameObject);
+        }
+    }
 }
