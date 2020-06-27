57579471e812db6cb06fc16475dbba0287b916fa
Alex Sampson <32361325+AlexSDevDump@users.noreply.github.com>
2020-06-27 12:21:24 +0100

    Level Manager

---
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public float remainingTime = 5;
+
+    private void Update()
+    {
+        if (remainingTime < 0)
+            remainingTime -= Time.deltaTime;
+    }
+}
